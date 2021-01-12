9e8899ebe3f93253c30e296d7f03b4a5947832ee
Flavio Silva <flavio@MacBook-Air-de-Flavio.local>
2021-01-12 16:16:30 -0300

    Criando tratamento de erros

--- app.js
+++ app.js
@@ -10,4 +10,20 @@ app.use(morgan("dev"));
 app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
 
+//Tratamento de erro para quando não encontrar nenhuma rota
+app.use((req, res, next) => {
+  const error = new Error("Rota não encontrada");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      mensagem: error.message,
+    },
+  });
+});
+
 module.exports = app;
