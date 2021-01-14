7e870439201511fed04f7f19eae1e59b4c8f5749
Flavio Silva <flavio@MacBook-Air-de-Flavio.local>
2021-01-14 17:42:56 -0300

    Criando pasta /config para express

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const app = require("./src/config/express");
 
-app.listen(port, () => {
-  console.log(`App rodando na porta ${port}`);
+app.listen(3000, () => {
+  console.log(`App rodando na porta 3000`);
 });
+
 app.get("/", (req, res) => {
   res.send("Hello word");
 });
