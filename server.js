b7db8b06a1d77c12991b9f21b22cdbb1c6d63247
Flavio Silva <contatoflaviodev@gmail.com>
2021-02-18 16:54:39 -0300

    Adicionando var de ambiente e docker-compose para subir DB

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-
 const app = require("./src/config/custom-express");
 
+const port = process.env.SERVER_PORT
+
 app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
+const user = process.env.DB_USER
+const pass = process.env.DB_PASS
+const host = process.env.DB_HOST
+const port = process.env.DB_POST
+const dbname = process.env.DB_NAME
+
+
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/noderest", {
+mongoose.connect(`mongodb://${user}:${pass}@${host}:${port}/${dbname}?authSource=admin`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(() => {
+  console.log("DB connected!")
+}).catch((error) => {
+  console.log(`Message: ${error.message}`)
+})
 
 module.exports = mongoose;
